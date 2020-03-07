06d4e94f973e72ceee1128a37d40df11779d00c2
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-03-06 16:18:51 -0800

    tmp

---
+++ src/helpers/decode.ts
@@ -0,0 +1,56 @@
+const fromCharCode = String.fromCharCode;
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+const ENTITY_PATTERN = /&#(\d+);|&#x([0-9a-f]+);|&(\w+);/ig;
+
+const ENTITY_MAPPING = {
+  amp: "&",
+  apos: "'",
+  gt: ">",
+  lt: "<",
+  quot: '"'
+};
+
+// map UPPERCASE variants of supported special chars
+Object.keys(ENTITY_MAPPING).forEach(function(k) {
+  ENTITY_MAPPING[k.toUpperCase()] = ENTITY_MAPPING[k];
+});
+
+
+function replaceEntities(_, d, x, z) {
+
+  // reserved names, i.e. &nbsp;
+  if (z) {
+    if (hasOwnProperty.call(ENTITY_MAPPING, z)) {
+      return ENTITY_MAPPING[z];
+    } else {
+      // fall back to original value
+      return "&" + z + ";";
+    }
+  }
+
+  // decimal encoded char
+  if (d) {
+    return fromCharCode(d);
+  }
+
+  // hex encoded char
+  return fromCharCode(parseInt(x, 16));
+}
+
+
+/**
+ * A basic entity decoder that can decode a minimal
+ * sub-set of reserved names (&amp;) as well as
+ * hex (&#xaaf;) and decimal (&#1231;) encoded characters.
+ *
+ * @param {string} str
+ *
+ * @return {string} decoded string
+ */
+export default function decodeEntities(s) {
+  if (s.length > 3 && s.indexOf("&") !== -1) {
+    return s.replace(ENTITY_PATTERN, replaceEntities);
+  }
+
+  return s;
+}
--- src/helpers/xmlParser.ts
+++ src/helpers/xmlParser.ts
@@ -1,64 +1,10 @@
-let fromCharCode = String.fromCharCode;
-let hasOwnProperty = Object.prototype.hasOwnProperty;
-let ENTITY_PATTERN = /&#(\d+);|&#x([0-9a-f]+);|&(\w+);/ig;
+import decodeEntities from "./decode";
 
-let ENTITY_MAPPING = {
-    amp: "&",
-    apos: "'",
-    gt: ">",
-    lt: "<",
-    quot: '"'
-};
+const XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
+const XSI_PREFIX = "xsi";
+const XSI_TYPE = "xsi:type";
 
-// map UPPERCASE variants of supported special chars
-Object.keys(ENTITY_MAPPING).forEach(function (k) {
-    ENTITY_MAPPING[k.toUpperCase()] = ENTITY_MAPPING[k];
-});
-
-function replaceEntities(_, d, x, z) {
-
-    // reserved names, i.e. &nbsp;
-    if (z) {
-        if (hasOwnProperty.call(ENTITY_MAPPING, z)) {
-            return ENTITY_MAPPING[z];
-        } else {
-            // fall back to original value
-            return "&" + z + ";";
-        }
-    }
-
-    // decimal encoded char
-    if (d) {
-        return fromCharCode(d);
-    }
-
-    // hex encoded char
-    return fromCharCode(parseInt(x, 16));
-}
-
-
-/**
- * A basic entity decoder that can decode a minimal
- * sub-set of reserved names (&amp;) as well as
- * hex (&#xaaf;) and decimal (&#1231;) encoded characters.
- *
- * @param {string} str
- *
- * @return {string} decoded string
- */
-function decodeEntities(s) {
-    if (s.length > 3 && s.indexOf("&") !== -1) {
-        return s.replace(ENTITY_PATTERN, replaceEntities);
-    }
-
-    return s;
-}
-
-let XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
-let XSI_PREFIX = "xsi";
-let XSI_TYPE = "xsi:type";
-
-let NON_WHITESPACE_OUTSIDE_ROOT_NODE = "non-whitespace outside of root node";
+const NON_WHITESPACE_OUTSIDE_ROOT_NODE = "non-whitespace outside of root node";
 
 function error(msg) {
     return new Error(msg);
@@ -1034,4 +980,5 @@ export function Parser(options): void {
             j += 1;
         }
     } /** end parse */
+
 }
